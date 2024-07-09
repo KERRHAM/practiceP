6f5c07688ca35aac9cf770fd6092a8a17f667ed9
Kerr Hamilton <kerr9928@gmail.com>
2024-07-09 21:00:49 +0000

    create more functions for userbility

--- battleship.py
+++ battleship.py
@@ -17,20 +17,45 @@ def show_board(hit,miss,comp):
             place = place + 1
         print(x," ", row)
 
-hit = [21, 22]
-miss = [20, 24, 25]
-comp = [23]
+boat1 = [25, 35, 45]
+boat2 = [61, 62, 63]
+hit = []
+miss = []
+comp = []
 
-def start_game():
+def start_game(strike_attempts):
 
     while True:
       strike = input(f"Please enter your Guess coordinates!!! ")
       strike = int(strike)
       if strike < 0 or strike > 99:
         print("Please enter a number between 0 and 99!!")
+      elif strike in strike_attempts:
+        print("You've already entered this coordinate, please try again!!!")
       else:
         break
     return strike
 
-start_game()
-show_board(hit, miss, comp)
+def check_strike(strike, boat1, hit, miss, comp):
+
+    if strike in boat1:
+        boat1.remove(strike)
+        if len(boat1) > 0:
+            hit.append(strike)
+        else:
+            comp.append(strike)
+    else:
+        miss.append(strike)
+        
+        return boat1, hit, miss, comp
+
+for i in range(5):
+    strike_attempts = hit + miss + comp
+    strike = start_game(strike_attempts)
+    check_strike(strike, boat1, hit, miss, comp)
+    show_board(hit, miss, comp)
+
+    if len(boat1) < 1:
+        print("Well done, you sunk my battleship!!!")
+        break
+    
